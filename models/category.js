cf7fe5f84bb3ae1a49f316bc62e4ce6bfb368591
dr96mabuza <dr96mabuza@gmail.com>
2022-07-15 10:59:02 +0200

    added schema

--- models/category.js
+++ models/category.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CategorySchema = new Schema({});
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+  num_of_items: { type: Number, required: true },
+  description: { type: String, required: true },
+});
+
+CategorySchema.virtual("url").get(() => {
+  return `/category/${this._id}`;
+});
 
 module.exports = mongoose.model("Category", CategorySchema);
--- models/item.js
+++ models/item.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ItemSchema = new Schema({});
+const ItemSchema = new Schema({
+  name: { type: String, required: true, maxLength: 50, minLength: 5 },
+  price: { type: Number, required: true, min: 1 },
+  description: { type: String, required: true },
+  num_in_stock: { type: Number, required: true },
+  category: [],
+});
+
+ItemSchema.virtual("url").get(() => {
+  return `/item/${this._id}`;
+});
 
 module.exports = mongoose.model("Item", ItemSchema);
