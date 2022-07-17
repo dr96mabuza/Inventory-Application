b57c84c460e16f56abc27802eac54a4274a6c19a
dr96mabuza <dr96mabuza@gmail.com>
2022-07-17 14:06:45 +0200

    added the files to database

--- populatedb.js
+++ populatedb.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 console.log(
-  "This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true"
+  "This script populates some test items and categories to my database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true"
 );
 
 // Get arguments passed on command line
@@ -26,6 +26,20 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 var items = [];
 var categories = [];
 
+function categoryCreate(name, description, cb) {
+  const category = new Category({ name: name, description: description });
+
+  category.save(function (err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log(`New Category: ${category}`);
+    categories.push(category);
+    cb(null, category);
+  });
+}
+
 function itemCreate(
   name,
   price,
@@ -39,12 +53,12 @@ function itemCreate(
     name: name,
     price: price,
     description: description,
-    studio,
+    studio: studio,
     num_in_stock: num_in_stock,
     category: category,
   };
 
-  var item = new Item(itemdetail);
+  const item = new Item(itemdetail);
 
   item.save(function (err) {
     if (err) {
@@ -57,86 +71,89 @@ function itemCreate(
   });
 }
 
-function categoryCreate(name, description) {
-  const category = new Category({ name: name, description: description });
-
-  category.save((err) => {
-    if (err) {
-      cb(err, null);
-      return;
-    }
-    console.log(`New Category: ${category}`);
-    categories.push(category);
-    cb(null, category);
-  });
-}
-
 function createItems(cb) {
-  async.series([
-    function (callback) {
-      itemCreate(
-        "Call of Duty: MW3",
-        1000,
-        "2011, single/multiplayer first person shooter game. modern setting.",
-        "ACTIVISION",
-        1,
-        [category[0]],
-        callback
-      );
-    },
-    function (callback) {
-      itemCreate(
-        `Assassins\'s Creed IV + Assassin\'s Creed Rouge`,
-        1200,
-        "2013, double disc, single/multiplayer first person, open world game. historical setting.",
-        "UBISOFT",
-        1,
-        [categories[1]],
-        callback
-      );
-    },
-    function (callback) {
-      itemCreate(
-        `BATTLEFIELD 3: limited edition`,
-        800,
-        "2011, single/multiplayer first person shooter game, modern setting.",
-        "DICE",
-        1,
-        [categories[0]],
-        callback
-      );
-    },
-    function (callback) {
-      itemCreate(
-        `Nedd for Speed: MOST WANTED`,
-        1100,
-        "2012, single player motor racing game, modern setting.",
-        "CRITERION",
-        2,
-        [categories[2]],
-        callback
-      );
-    },
+  async.series(
+    [
+      function (callback) {
+        itemCreate(
+          "Call of Duty: MW3",
+          1000,
+          "2011, single/multiplayer first person shooter game. modern setting.",
+          "ACTIVISION",
+          1,
+          [categories[0]],
+          callback
+        );
+      },
+      function (callback) {
+        itemCreate(
+          `Assassins\'s Creed IV + Assassin\'s Creed Rouge`,
+          1200,
+          "2013, double disc, single/multiplayer first person, open world game. historical setting.",
+          "UBISOFT",
+          1,
+          [categories[1]],
+          callback
+        );
+      },
+      function (callback) {
+        itemCreate(
+          "BATTLEFIELD 3: limited edition",
+          800,
+          "2011, single/multiplayer first person shooter game, modern setting.",
+          "DICE",
+          1,
+          [categories[0]],
+          callback
+        );
+      },
+      function (callback) {
+        itemCreate(
+          `Nedd for Speed: MOST WANTED`,
+          1100,
+          "2012, single player motor racing game, modern setting.",
+          "CRITERION",
+          2,
+          [categories[2]],
+          callback
+        );
+      },
+    ],
     cb
-  ]);
+  );
 }
 
 function createCategories(cb) {
-  async.waterfall([
-    function (callback) {
-      categoryCreate("First person shooter", "", callback);
-    },
-    function (callback) {
-      categoryCreate("Open World/ Historical", "", callback);
-    },
-    function (callback) {
-      categoryCreate("Motorsport", "", callback);
-    },
+  async.parallel(
+    [
+      function (callback) {
+        categoryCreate(
+          "First person shooter",
+          `a type of video game whose gameplay involves shooting enemies and other targets and in which a player views the action as though through the eyes of the character they are controlling.
+        \"blast through hordes of aliens in this sci-fi first-person shooter\"`,
+          callback
+        );
+      },
+      function (callback) {
+        categoryCreate(
+          "Action Adventure/Historical",
+          "An action adventure game can be defined as a game with a mix of elements from an action game and an adventure game, especially crucial elements like puzzles.",
+          callback
+        );
+      },
+      function (callback) {
+        categoryCreate(
+          "Racing",
+          "Racing games are a video game genre in which the player participates in a racing competition. They may be based on anything from real-world racing leagues to fantastical settings. They are distributed along a spectrum between more realistic racing simulations and more fantastical arcade-style racing games",
+          callback
+        );
+      },
+    ],
     cb
-  ]);
+  );
 }
 
-async.series(
+async.parallel(
   [createCategories, createItems],
   // Optional callback
   function (err, results) {
