c61da436622dded798e1cd6476491b9028aeb1ad
dr96mabuza <dr96mabuza@gmail.com>
2022-07-17 16:24:39 +0200

    created controllers for item and category

---
+++ controllers/categoryController.js
@@ -0,0 +1,41 @@
+const Category = require("../models/category");
+
+//display list of all categoriess
+exports.category_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: category list");
+};
+
+//detailed page of specific category
+exports.category_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED: category detail");
+};
+
+//display category create form on GET
+exports.category_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: category create GET");
+};
+
+//handle category create on POST
+exports.category_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: category create POST");
+};
+
+//Display category delete form on GET
+exports.category_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: category delete GET");
+};
+
+//Handle category delete on POST
+exports.category_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: category delete POST");
+};
+
+//Display category update form on GET
+exports.category_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: category update GET");
+};
+
+//handle category update on POST
+exports.category_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: category update POST");
+};
---
+++ controllers/itemController.js
@@ -0,0 +1,41 @@
+const Item = require("../models/item");
+
+//display list of all items
+exports.item_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item list");
+};
+
+//detailed page of specific item
+exports.item_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item detail");
+};
+
+//display item create form on GET
+exports.item_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item create GET");
+};
+
+//handle Item create on POST
+exports.item_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item create POST");
+};
+
+//Display item delete form on GET
+exports.item_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item delete GET");
+};
+
+//Handle item delete on POST
+exports.item_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item delete POST");
+};
+
+//Display Item update form on GET
+exports.item_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item update GET");
+};
+
+//handle item update on POST
+exports.item_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Item update POST");
+};
